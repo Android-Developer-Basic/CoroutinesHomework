50964ffab412bf7fa43165ded6f752bb2e6efa87
Ivan Toropygin <itori86@mail.ru>
2025-01-22 15:45:59 +0300

    Task 2. Login

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -3,25 +3,50 @@ package ru.otus.coroutineshomework.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.otus.coroutineshomework.ui.login.data.Credentials
 
 class LoginViewModel : ViewModel() {
+    private val loginApi = LoginApi()
 
     private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
     val state: LiveData<LoginViewState> = _state
 
-    /**
-     * Login to the network
-     * @param name user name
-     * @param password user password
-     */
     fun login(name: String, password: String) {
-        // TODO: Implement login
+
+        _state.value = LoginViewState.LoggingIn
+
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val user = loginApi.login(Credentials(name, password))
+                withContext(Dispatchers.Main) {
+                    _state.value = LoginViewState.Content(user)
+                }
+            } catch (exception: Exception) {
+                withContext(Dispatchers.Main) {
+                    _state.value = LoginViewState.Login(exception)
+                }
+            }
+        }
     }
 
-    /**
-     * Logout from the network
-     */
     fun logout() {
-        // TODO: Implement logout
+        _state.value = LoginViewState.LoggingOut
+
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                loginApi.logout()
+                withContext(Dispatchers.Main) {
+                    _state.value = LoginViewState.Login()
+                }
+            } catch (exception: Exception) {
+                withContext(Dispatchers.Main) {
+                    _state.value = LoginViewState.Login(exception)
+                }
+            }
+        }
     }
 }
