4fc9e9f8df0650580f5e0a0e209e2d4007e5be54
SpiritualAdviser <pomaxam@narod.ru>
2025-01-26 14:20:45 +0300

    2 task is done

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -3,11 +3,17 @@ package ru.otus.coroutineshomework.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.otus.coroutineshomework.ui.login.data.Credentials
+import ru.otus.coroutineshomework.ui.login.data.User
 
 class LoginViewModel : ViewModel() {
 
     private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
     val state: LiveData<LoginViewState> = _state
+    private val loginAIp = LoginApi()
 
     /**
      * Login to the network
@@ -15,13 +21,27 @@ class LoginViewModel : ViewModel() {
      * @param password user password
      */
     fun login(name: String, password: String) {
-        // TODO: Implement login
+
+        viewModelScope.launch(Dispatchers.IO) {
+            _state.postValue(LoginViewState.LoggingIn)
+
+            try {
+                val userNetworkRequest = loginAIp.login(Credentials(name, password))
+                _state.postValue(LoginViewState.Content(userNetworkRequest))
+            } catch (e: Exception) {
+                _state.postValue(LoginViewState.Login(e))
+            }
+        }
     }
 
     /**
      * Logout from the network
      */
     fun logout() {
-        // TODO: Implement logout
+        viewModelScope.launch(Dispatchers.IO) {
+            _state.postValue(LoginViewState.LoggingOut)
+            loginAIp.logout()
+            _state.postValue(LoginViewState.Login())
+        }
     }
 }
