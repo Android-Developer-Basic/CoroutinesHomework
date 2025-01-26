2d9f73f91a4a41298079691988e39c65e0b73db4
Ekaterina Leonidova <leonidovaes@wand.am>
2025-01-26 13:40:28 +0300

    task 2.1 login with coroutines

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -3,11 +3,17 @@ package ru.otus.coroutineshomework.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.otus.coroutineshomework.ui.login.data.Credentials
 
 class LoginViewModel : ViewModel() {
 
     private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
     val state: LiveData<LoginViewState> = _state
+    private val loginApi = LoginApi()
 
     /**
      * Login to the network
@@ -15,13 +21,35 @@ class LoginViewModel : ViewModel() {
      * @param password user password
      */
     fun login(name: String, password: String) {
-        // TODO: Implement login
+        _state.value = LoginViewState.LoggingIn
+        viewModelScope.launch {
+            try {
+                val user = withContext(Dispatchers.IO) {
+                    return@withContext loginApi.login(Credentials(name, password))
+                }
+                _state.value = LoginViewState.Content(user)
+            } catch (e: Exception) {
+                _state.value = LoginViewState.Login(e)
+            }
+        }
     }
 
     /**
      * Logout from the network
      */
     fun logout() {
-        // TODO: Implement logout
+        _state.value = LoginViewState.LoggingOut
+
+        viewModelScope.launch {
+            try {
+                withContext(Dispatchers.IO) {
+                    return@withContext loginApi.logout()
+                }
+                _state.value = LoginViewState.Login()
+            } catch (e: Exception) {
+                _state.value = LoginViewState.Login(e)
+            }
+        }
+
     }
 }
