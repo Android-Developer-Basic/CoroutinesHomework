20434aae6fa7d7441e88e526e663005753a80ba0
atuzhilov <atuzhilov@tradingview.com>
2025-01-26 17:23:42 +0300

    2. Login flow

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -1,10 +1,13 @@
 package ru.otus.coroutineshomework.ui.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.otus.coroutineshomework.ui.login.data.Credentials
@@ -12,34 +15,38 @@ import ru.otus.coroutineshomework.ui.login.data.User
 
 class LoginViewModel : ViewModel() {
 
-    private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
-    val state: LiveData<LoginViewState> = _state
+    private val stateFlow = MutableStateFlow<LoginViewState>(LoginViewState.Login())
+    val state: StateFlow<LoginViewState> = stateFlow
     private val loginApi = LoginApi()
     private lateinit var user: User
 
     fun login(name: String, password: String) {
-        _state.value = LoginViewState.LoggingIn
+        stateFlow.value = LoginViewState.LoggingIn
         viewModelScope.launch {
-            try {
-                withContext(Dispatchers.IO) {
-                    user = loginApi.login(Credentials(name, password))
-                }
-                _state.value = LoginViewState.Content(user)
-            } catch (e: IllegalArgumentException) {
-                withContext(Dispatchers.Main) {
-                    _state.value = LoginViewState.Login(e)
-                }
+            loginFlow(name, password).collect {
+                stateFlow.value = it
             }
         }
     }
 
     fun logout() {
-        _state.value = LoginViewState.LoggingOut
+        stateFlow.value = LoginViewState.LoggingOut
         viewModelScope.launch {
             withContext(Dispatchers.IO) {
                 loginApi.logout()
             }
-            _state.value = LoginViewState.Login()
+            stateFlow.value = LoginViewState.Login()
         }
     }
+
+    private fun loginFlow(name: String, password: String): Flow<LoginViewState> = flow {
+        emit(LoginViewState.LoggingIn)
+        try {
+            user = loginApi.login(Credentials(name, password))
+            emit(LoginViewState.Content(user))
+        } catch (e: IllegalArgumentException) {
+            emit(LoginViewState.Login(e))
+
+        }
+    }.flowOn(Dispatchers.IO)
 }
