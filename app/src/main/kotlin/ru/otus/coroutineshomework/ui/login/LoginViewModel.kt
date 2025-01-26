9eb7cb27bed667b386e95eafeed978eb96dd310b
Ekaterina Leonidova <leonidovaes@wand.am>
2025-01-26 15:22:02 +0300

    task 2.2 login with flow

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -5,14 +5,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.otus.coroutineshomework.ui.login.data.Credentials
 
 class LoginViewModel : ViewModel() {
 
-    private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
-    val state: LiveData<LoginViewState> = _state
+    private val _state = MutableStateFlow<LoginViewState>(LoginViewState.Login())
+    val state: StateFlow<LoginViewState> = _state
     private val loginApi = LoginApi()
 
     /**
@@ -21,16 +25,22 @@ class LoginViewModel : ViewModel() {
      * @param password user password
      */
     fun login(name: String, password: String) {
-        _state.value = LoginViewState.LoggingIn
         viewModelScope.launch {
-            try {
-                val user = withContext(Dispatchers.IO) {
-                    return@withContext loginApi.login(Credentials(name, password))
-                }
-                _state.value = LoginViewState.Content(user)
-            } catch (e: Exception) {
-                _state.value = LoginViewState.Login(e)
+            loginFlow(Credentials(name, password)).collect {
+                _state.value = it
             }
+        }
+    }
+
+    private fun loginFlow(creds: Credentials): Flow<LoginViewState> = flow {
+        emit(LoginViewState.LoggingIn)
+        try {
+            val user = withContext(Dispatchers.IO) {
+                return@withContext loginApi.login(creds)
+            }
+            emit(LoginViewState.Content(user))
+        } catch (e: Exception) {
+            emit(LoginViewState.Login(e))
         }
     }
 
